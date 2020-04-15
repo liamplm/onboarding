be1749a1156bf8bd6ca3469aad35c7247ffd2ef8
liamplm <liamplm82@gmail.com>
2020-04-16 03:13:34 +0430

    fix error handling

--- scripts/run_in_file_program.sh
+++ scripts/run_in_file_program.sh
@@ -1,11 +1,13 @@
-#set -eu
+set -eu
 BASE_PATH=$(pwd)
 
 cd workspace/$CHALLENGE_NAME
 mkdir data/
 cp $BASE_PATH/challenges/$CHALLENGE_NAME/input.txt data/input.txt
 
+set +e
 docker container rm "$CHALLENGE_NAME-container" &> /dev/null
+set -eu
 
 COMMAND=$(docker inspect prime --format='{{.ContainerConfig.Entrypoint}}')
 CMD=${COMMAND:1:$((${#COMMAND} - 2))} #remove first and last char (long version to work on both Linux and Mac)
